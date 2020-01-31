022c46a49a41fa61df3a4ce3d4973a1c65f741c9
Romain <r.chevallier21@hotmail.fr>
2020-01-31 10:33:54 +0100

    [Web] Update hosted website with last tutorial

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,16 +1,16 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "b4d8bfd922da5c9dc6b7aa290d11680a",
-"/assets\assets\fonts\OpenSans-ExtraBold.ttf": "fb7e3a294cb07a54605a8bb27f0cd528",
-"/assets\assets\fonts\OpenSans-Regular.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4",
-"/assets\assets\logo.png": "e92d75da68b0db510c0ce50855b2de47",
-"/assets\FontManifest.json": "b3d1c9ea0c36fce7b56886e3213c291d",
+  "/assets\AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"/assets\FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "512e08a124df968d4b5d3e09d9450e69",
-"/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
-"/index.html": "b54b58a8768c6821b4448ef7db9dfc46",
-"/main.dart.js": "facc2e37994c1ec2a57b3c8eab9a56c0"
+"/assets\LICENSE": "80b91826d96855c975080453445351d1",
+"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/index.html": "f21529d74c954b9d5bf31345b9056380",
+"/main.dart.js": "bf1d14c8ea4650059d0e00f87a29c72e",
+"/manifest.json": "53c1d7421d93bca93d0fcb1678c5a632"
 };
 
 self.addEventListener('activate', function (event) {
