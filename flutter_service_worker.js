0f351944456626300413cf59f54d5f72aba86b0c
Romain <r.chevallier21@hotmail.fr>
2020-02-07 23:36:50 +0100

    [Web] update hosted website with last  changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,15 +1,16 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"/assets\FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "80b91826d96855c975080453445351d1",
+  "/assets/AssetManifest.json": "45a0bc6a3375c09c92551184b681503e",
+"/assets/assets/img/icons/flutter_logo.png": "13e9c72ec37fac220397aa819fa1ef2d",
+"/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/LICENSE": "6a8f64e8c4c77609ee048c83533db70b",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "f21529d74c954b9d5bf31345b9056380",
-"/main.dart.js": "bf1d14c8ea4650059d0e00f87a29c72e",
+"/main.dart.js": "28c83931bad6cfe242c21d60169e3e33",
 "/manifest.json": "53c1d7421d93bca93d0fcb1678c5a632"
 };
 
@@ -32,9 +33,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
