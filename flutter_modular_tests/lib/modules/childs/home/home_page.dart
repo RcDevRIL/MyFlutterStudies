a2345ad78451c4a8b1b0e286cbeb7edff5aef298
Romain <r.chevallier21@hotmail.fr>
2020-02-07 23:35:08 +0100

    [flutter_modular] begin to merge projects

--- flutter_modular_tests/lib/modules/childs/home/home_page.dart
+++ flutter_modular_tests/lib/modules/childs/home/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_modular/flutter_modular.dart' show Consumer;
 import 'package:flutter_modular_tests/modules/childs/home/home_bloc.dart'
     show HomeBloc;
 import 'package:flutter_modular_tests/modules/childs/home/home_module.dart';
+import 'package:flutter_modular_tests/widgets/navigation_bar/navigation_bar.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -16,29 +18,39 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+        drawer:
+            /*  sizingInformation.deviceScreenType == DeviceScreenType.Mobile
+            ? NavigationDrawer()
+            :  */
+            null,
+        body: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            NavigationBar(),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Consumer<HomeBloc>(
+                      builder: (context, value) => Text(
+                            '${value.counter}',
+                            style: Theme.of(context).textTheme.headline4,
+                          )),
+                ],
+              ),
             ),
-            Consumer<HomeBloc>(
-                builder: (context, value) => Text(
-                      '${value.counter}',
-                      style: Theme.of(context).textTheme.headline4,
-                    )),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Increment',
-        onPressed: () => HomeModule.to.get<HomeBloc>().increment(),
-        child: Icon(Icons.add),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Increment',
+          onPressed: () => HomeModule.to.get<HomeBloc>().increment(),
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
