0a971905b7157401de32de3cc6c9e489a9bbcfba
Romain <r.chevallier21@hotmail.fr>
2020-02-08 10:46:01 +0100

    [Animations] begin work on a custom loading button
    Goal ?
    Reproduce this:
    - https://github.com/leandroBorgesFerreira/LoadingButtonAndroid

---
+++ loading_button/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  // Enable integration testing with the Flutter Driver extension.
+  // See https://flutter.dev/testing/ for more info.
+  enableFlutterDriverExtension();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            CustomLoadingButton(),
+            Column(
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CustomLoadingButton extends StatelessWidget {
+  const CustomLoadingButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {},
+      color: Colors.blueGrey,
+      elevation: 2.0,
+      focusElevation: 1.0,
+      hoverColor: Colors.lightBlueAccent,
+      shape: RoundedRectangleBorder(),
+      textColor: Colors.white,
+      child: Text('My button is awesome'),
+    );
+  }
+}
