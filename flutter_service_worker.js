a31e68c1a76a88cfdb7237b567b81ffa632f6deb
Romain <r.chevallier21@hotmail.fr>
2020-02-11 22:53:07 +0100

    [Web] Update website with last changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,12 +5,12 @@ const RESOURCES = {
 "/assets/assets/img/icons/flutter_logo.png": "13e9c72ec37fac220397aa819fa1ef2d",
 "/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "6a8f64e8c4c77609ee048c83533db70b",
+"/assets/LICENSE": "5e1fcffa37828d058f9a43a59973a043",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "f21529d74c954b9d5bf31345b9056380",
-"/main.dart.js": "28c83931bad6cfe242c21d60169e3e33",
+"/index.html": "53b2cd03801486ead76016a5fc5078d3",
+"/main.dart.js": "3be8e71ca013cf6b7afb615943e6fda2",
 "/manifest.json": "53c1d7421d93bca93d0fcb1678c5a632"
 };
 
