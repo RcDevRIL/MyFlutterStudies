7ac7e1229480959a696a5e0fad6b3253f8e68a9a
Romain <r.chevallier21@hotmail.fr>
2020-02-12 12:34:22 +0100

    [Web] Update website with last changes
    - v0.1 PRE-RELEASE

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,12 +5,12 @@ const RESOURCES = {
 "/assets/assets/img/icons/flutter_logo.png": "13e9c72ec37fac220397aa819fa1ef2d",
 "/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "5e1fcffa37828d058f9a43a59973a043",
+"/assets/LICENSE": "21dcab1ca692090f1da2af1354ccfdaa",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "53b2cd03801486ead76016a5fc5078d3",
-"/main.dart.js": "3be8e71ca013cf6b7afb615943e6fda2",
+"/main.dart.js": "f9700e5a404bd73bb0d5887b306ca5f7",
 "/manifest.json": "53c1d7421d93bca93d0fcb1678c5a632"
 };
 
